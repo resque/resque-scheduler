6efc07a1ca15f6eb4fc51a793fe5832edc53e4cc
Russell Branca <chewbranca@gmail.com>
2010-08-05 03:25:02 +0800

    Adding environment load options for a resque initializer file

--- lib/resque_scheduler/tasks.rb
+++ lib/resque_scheduler/tasks.rb
@@ -5,12 +5,18 @@ namespace :resque do
   task :setup
 
   desc "Start Resque Scheduler"
-  task :scheduler => :setup do
+  task :scheduler => :scheduler_setup do |t,args|
     require 'resque'
     require 'resque_scheduler'
 
     Resque::Scheduler.verbose = true if ENV['VERBOSE']
     Resque::Scheduler.run
+  end
+
+  # task :scheduler_setup => :setup
+  task :scheduler_setup do
+    path = ENV['load_path']
+    load path.to_s.strip if path
   end
 
 end
