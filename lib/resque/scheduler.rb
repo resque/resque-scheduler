8b8b4e2a25df68a31e1555704dc8ae0b7b098ffd
trogdoro <nospam@nospam.com>
2011-07-19 14:07:48 -0700

    Use Resque.enqueue_with_queue to respect passing queue names.  Uses feature in https://github.com/demandchain/resque/commit/1c90a381283a15a4818e2c0e70860e0d9172e869.  Pending pull request to defunkt.

--- lib/resque/scheduler.rb
+++ lib/resque/scheduler.rb
@@ -198,7 +198,7 @@ module Resque
           # for non-existent classes (for example: running scheduler in
           # one app that schedules for another
           if Class === klass
-            Resque.enqueue(klass, *params)
+            Resque.enqueue_with_queue(queue, klass, *params)
           else
             Resque::Job.create(queue, klass, *params)
           end
