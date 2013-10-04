ce9b0033b084dbc3923c367d5065f5e07fe08ef3
bbauer <brian.bauer@g5platform.com>
2013-10-04 15:30:18 -0700

    Use new Rufus Scheduler initialization syntax

--- lib/resque/scheduler.rb
+++ lib/resque/scheduler.rb
@@ -243,7 +243,7 @@ module Resque
       end
 
       def rufus_scheduler
-        @rufus_scheduler ||= Rufus::Scheduler.start_new
+        @rufus_scheduler ||= Rufus::Scheduler.new
       end
 
       # Stops old rufus scheduler and creates a new one.  Returns the new
