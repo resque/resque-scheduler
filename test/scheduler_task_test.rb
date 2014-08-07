a0896447453d85e8a7c307179d69ba1f703ee7bb
errant01 <errant01@gmail.com>
2014-08-07 16:27:28 -0500

    Issue 434 - adding test for breaking out when poll_sleep_amount = 0

--- test/scheduler_task_test.rb
+++ test/scheduler_task_test.rb
@@ -37,4 +37,22 @@ context 'Resque::Scheduler' do
     Resque::Scheduler.unstub(:release_master_lock!)
     Resque::Scheduler.release_master_lock!
   end
+
+  test 'sending TERM to scheduler breaks out when poll_sleep_amount = 0' do
+    Resque::Scheduler.poll_sleep_amount = 0
+    Resque::Scheduler.expects(:release_master_lock!)
+
+    @pid = Process.pid
+    Thread.new do
+      sleep(0.05)
+      Process.kill(:TERM, @pid)
+    end
+
+    assert_raises SystemExit do
+      Resque::Scheduler.run
+    end
+
+    Resque::Scheduler.unstub(:release_master_lock!)
+    Resque::Scheduler.release_master_lock!
+  end
 end
