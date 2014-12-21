45ddb5f1ab7f9a9674bcbde7ce1dfdf44a51e0e1
Dan Buch <daniel.buch@gmail.com>
2014-12-21 12:33:08 -0500

    Replacing custom pidfile cleanup with at_exit block
    and removing the in-process test.  Best effort is best effort.

--- lib/resque/scheduler/env.rb
+++ lib/resque/scheduler/env.rb
@@ -25,31 +25,21 @@ module Resque
 
       private
 
-      # Returns a proc, that when called will attempt to delete the given file.
-      # This is because implementing ObjectSpace.define_finalizer is tricky.
-      # Hat-Tip to @mperham for describing in detail:
-      # http://www.mikeperham.com/2010/02/24/the-trouble-with-ruby-finalizers/
-      def self.pidfile_deleter(pidfile_path)
-        proc do
-          File.delete(pidfile_path) if File.exist?(pidfile_path)
-        end
-      end
-
-      attr_reader :options
+      attr_reader :options, :pidfile_path
 
       def setup_backgrounding
+        return unless options[:background]
+
         # Need to set this here for conditional Process.daemon redirect of
         # stderr/stdout to /dev/null
         Resque::Scheduler.quiet = !!options[:quiet]
 
-        if options[:background]
-          unless Process.respond_to?('daemon')
-            abort 'background option is set, which requires ruby >= 1.9'
-          end
-
-          Process.daemon(true, !Resque::Scheduler.quiet)
-          Resque.redis.client.reconnect
+        unless Process.respond_to?('daemon')
+          abort 'background option is set, which requires ruby >= 1.9'
         end
+
+        Process.daemon(true, !Resque::Scheduler.quiet)
+        Resque.redis.client.reconnect
       end
 
       def setup_pid_file
@@ -57,13 +47,11 @@ module Resque
 
         @pidfile_path = File.expand_path(options[:pidfile])
 
-        File.open(@pidfile_path, 'w') do |f|
+        File.open(pidfile_path, 'w') do |f|
           f.puts $PROCESS_ID
         end
 
-        ObjectSpace.define_finalizer(
-          self, Env.pidfile_deleter(@pidfile_path)
-        )
+        at_exit { cleanup_pid_file }
       end
 
       def setup_scheduler_configuration
@@ -99,10 +87,9 @@ module Resque
       end
 
       def cleanup_pid_file
-        return unless @pidfile_path
+        return unless pidfile_path
 
-        ObjectSpace.undefine_finalizer(self)
-        Env.pidfile_deleter(@pidfile_path).call
+        File.delete(pidfile_path) if File.exist?(pidfile_path)
         @pidfile_path = nil
       end
     end
