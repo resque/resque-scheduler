f48a827ec6d7706a1b1f7ed513dfc95e48ae391b
dtaniwaki <daisuketaniwaki@gmail.com>
2015-04-19 10:35:45 +0900

    Avoid making keys for nil environment vairables

--- lib/resque/scheduler/cli.rb
+++ lib/resque/scheduler/cli.rb
@@ -136,7 +136,9 @@ module Resque
 
       def options
         @options ||= {}.tap do |o|
-          CLI_OPTIONS_ENV_MAPPING.map { |key, envvar| o[key] = env[envvar] }
+          CLI_OPTIONS_ENV_MAPPING.each do |key, envvar|
+            o[key] = env[envvar] if env.include?(envvar)
+          end
         end
       end
     end
