c9af191bea76f0522ae9a72710d44e3ed29e42b3
Carson Reinke <carson@reinke.co>
2017-11-16 13:20:43 -0500

    Force Travis to update Redis assert is for boolean, not nil Rubocop complaint

--- test/resque-web_test.rb
+++ test/resque-web_test.rb
@@ -101,7 +101,7 @@ context 'on GET to /delayed' do
     test "contains link to all schedules for class #{job['class']}" do
       Resque.enqueue_at(job['t'], job['class'], *job['args'])
       get '/delayed'
-      assert last_response.body =~ %r{/delayed/jobs/#{URI.escape(job['class'].to_s)}}
+      assert !(last_response.body =~ %r{/delayed/jobs/#{URI.escape(job['class'].to_s)}}).nil?
     end
   end
 end
