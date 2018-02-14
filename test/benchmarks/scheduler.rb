640b8a84eaa4b45a88928e3d5e4a8c2620f16632
Hormoz K <hkheradm@gmail.com>
2018-02-14 00:25:18 -0500

    modify bench

--- test/benchmarks/scheduler.rb
+++ test/benchmarks/scheduler.rb
@@ -10,7 +10,7 @@ context 'Resque::Scheduler' do
   end
 
   test 'testing with TEST_WITH_MIGRATOR is actually doing something' do
-    10_000.times { Resque.enqueue_in_with_queue(:default, 0, SomeJob) }
+    5_000.times { Resque.enqueue_in_with_queue(:default, 0, SomeJob) }
     StackProf.run(mode: :cpu, raw: true, out: 'stackprof.dump') do
       puts Benchmark.measure { Resque::Scheduler.handle_delayed_items }
     end
