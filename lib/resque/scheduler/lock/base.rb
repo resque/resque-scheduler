a181be10b77949312187461e48e048b8569eb1b5
Tyler Diaz <me@tylerdiaz.com>
2023-01-20 18:13:24 -0800

    Replace gethostbyname with Addrinfo.getaddrinfo
    This addresses the depreciation warning:
    
    ```
    /gems/resque-scheduler-4.5.0/lib/resque/scheduler/lock/base.rb:50: warning: Socket.gethostbyname is deprecated; use Addrinfo.getaddrinfo instead.
    ```

--- lib/resque/scheduler/lock/base.rb
+++ lib/resque/scheduler/lock/base.rb
@@ -47,7 +47,7 @@ module Resque
 
         def hostname
           local_hostname = Socket.gethostname
-          Socket.gethostbyname(local_hostname).first
+          Addrinfo.getaddrinfo(local_hostname, nil).first.getnameinfo.first
         rescue
           local_hostname
         end
