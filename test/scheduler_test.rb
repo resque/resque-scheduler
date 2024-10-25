b88d6a38ee4ff13d3832b3b6ab24824e0fba6915
Alex Schmitt <alex@alchemist.codes>
2024-10-25 10:55:08 -0700

    Fix `Resque::Scheduler.print_schedule`
    Added in https://github.com/resque/resque-scheduler/commit/893f13cfde48b1634faef8a870b134e8e9757661, this is intended to help with debugging. The `:t` and `:last` attrs were removed in the https://github.com/jmettraux/rufus-scheduler/compare/v2.0.24...v3.0.0
    
    It seems `#print_schedule` likely hasn't worked since the `rufus-scheduler` depenency was bumped to require ~3.0 in https://github.com/resque/resque-scheduler/commit/d24657e2b2babeb660ebfceb2d78b0e755199c33#diff-9732d81ad66d6656923a3883e83f6620a3b16a07484745677a0776f6e0bff025.
    
    This fixes the `OpenStruct` warnings seen in tests for newer Ruby versions.

--- test/scheduler_test.rb
+++ test/scheduler_test.rb
@@ -589,15 +589,15 @@ context 'Resque::Scheduler' do
 
   context 'printing schedule' do
     setup do
-      Resque::Scheduler.expects(:log!).at_least_once
+      Resque::Scheduler.stubs(:log!)
     end
 
     test 'prints schedule' do
-      fake_rufus_scheduler = mock
-      fake_rufus_scheduler.expects(:jobs).at_least_once
-                          .returns(foo: OpenStruct.new(t: nil, last: nil))
-      Resque::Scheduler.expects(:rufus_scheduler).at_least_once
-                       .returns(fake_rufus_scheduler)
+      rufus_scheduler = Rufus::Scheduler.new
+      fake_job = rufus_scheduler.at(Time.now + 1, job: true) {}
+      Resque::Scheduler.expects(:rufus_scheduler).at_least_once.returns(rufus_scheduler)
+      Resque::Scheduler.expects(:log!).with("#{fake_job.opts}\t#{fake_job.last_time}\t")
+
       Resque::Scheduler.print_schedule
     end
   end
